67fe5ec900c3c2021f0960f39fdd90be5bf813fb
GentleWinter <winter.alisson@gmail.com>
2024-05-11 14:56:00 -0300

    updates
    Signed-off-by: GentleWinter <winter.alisson@gmail.com>

---
+++ TutorApi/TutorApi.Domain/Entities/Tutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorApi.Domain.Entities
+{
+    public class Tutor
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateOnly dataNasc { get; set; }
+        public string email { get; set; }
+        public string documento { get; set; }
+    }
+}
